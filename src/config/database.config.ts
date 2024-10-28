fcb72857cf656562300a00cc6b6554b82a4db2e2
FahmiBahtiar <fahmibahtiar76@gmail.com>
2024-10-28 15:32:19 +0700

    switch to env

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://blimbing:blimbing@codemarket-shard-00-00.ribpd.mongodb.net:27017,codemarket-shard-00-01.ribpd.mongodb.net:27017,codemarket-shard-00-02.ribpd.mongodb.net:27017/backend-iot?ssl=true&replicaSet=atlas-di072z-shard-0&authSource=admin&retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGODB_URI || '');
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
